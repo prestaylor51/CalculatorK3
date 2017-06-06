5d6bc17df46287fccf72d6f7f94a87056049b585
prestaylor51 <prestaylor51@gmail.com>
2017-06-06 14:30:35 -0600

    just barely getting started

--- app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
+++ app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
@@ -1,7 +1,8 @@
 package com.taylor.preston.calculatork3;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
