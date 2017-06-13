605111fd61948f254f19d38312916ca7faf70044
prestaylor51 <prestaylor51@gmail.com>
2017-06-13 12:52:52 -0600

    the 0 button will print in the textView

--- app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
+++ app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
@@ -3,10 +3,13 @@ package com.taylor.preston.calculatork3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String equation;
+    public String equation = "";
+
 
 
     @Override
@@ -14,4 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handleButton(View view) {
+
+        equation += "0 ";
+        updateEquation(view);
+
+    }
+
+    public void updateEquation(View view) {
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(equation);
+
+    }
+
 }
