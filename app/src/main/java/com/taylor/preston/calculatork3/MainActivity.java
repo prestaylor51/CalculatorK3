9d89e0f97e722aa28227815588ddaa8c4afe66a1
prestaylor51 <prestaylor51@gmail.com>
2017-06-13 22:36:41 -0600

    formatted

--- app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
+++ app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
@@ -4,6 +4,7 @@ package com.taylor.preston.calculatork3;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
     public String opnd1 = "";
     public String opnd2 = "";
     public String operator = "";
+
     public Integer result = 0;
+    public Integer remainder = 0;
 
     private boolean isOp = false;
-
-
-
+    private boolean isEval = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,22 +29,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleButton(View view) {
+        if (!isEval) {
+            // Get the button based on id
+            Button b = (Button)view;
+            String bText = b.getText().toString();
 
-        // Get the button based on id
-        Button b = (Button)view;
-        String bText = b.getText().toString();
+            // If on first operand
+            if (!isOp) {
+                opnd1 += bText;
+            }
+            else {
+                opnd2 += bText;
+            }
 
-        // If on first opperand
-        if (!isOp) {
-            opnd1 += bText;
-        }
-        else {
-            opnd2 += bText;
-        }
+            // Concat the string with correct number/operation
 
-        // Concat the string with correct number/operation
-        equation += bText;
-        updateEquation(view);
+                equation += bText;
+                updateEquation(view);
+            }
 
     }
 
@@ -55,16 +58,12 @@ public class MainActivity extends AppCompatActivity {
             String bText = b.getText().toString();
             operator = bText;
 
-            equation += bText;
+            equation += " " + bText + " ";
 
             updateEquation(view);
 
             isOp = true;
         }
-
-    }
-
-    public void clearEquation(View view) {
 
     }
 
@@ -76,23 +75,36 @@ public class MainActivity extends AppCompatActivity {
      *          https://stackoverflow.com/questions/3422673/evaluating-a-math-expression-given-in-string-form
      */
     public void evaluateEquation (View view){
+        if (!isEval) {
+            switch (operator) {
+                case "+":
+                    result = Integer.parseInt(opnd1) + Integer.parseInt(opnd2);
+                    break;
+                case "-":
+                    result = Integer.parseInt(opnd1) - Integer.parseInt(opnd2);
+                    break;
+                case "×":
+                    result = Integer.parseInt(opnd1) * Integer.parseInt(opnd2);
+                    break;
+                case "÷":
+                    result = Integer.parseInt(opnd1) / Integer.parseInt(opnd2);
+                    remainder = Integer.parseInt(opnd1) % Integer.parseInt(opnd2);
+                    break;
+                default:
+                    result = 333;
+                    break;
+            }
 
-        switch (operator) {
-            case "+":
-                result = Integer.parseInt(opnd1) + Integer.parseInt(opnd2);
-                break;
-            case "-":
-                result = Integer.parseInt(opnd1) - Integer.parseInt(opnd2);
-                break;
-            default:
-                result = 333;
-                break;
+
+            equation += " = ";
+            equation += Integer.toString(result);
+
+            if (remainder > 0) {
+                equation += " r " + Integer.toString(remainder);
+            }
+            updateEquation(view);
         }
-
-        equation += " = ";
-        equation += Integer.toString(result);
-
-        updateEquation(view);
+        isEval = true;
     }
 
     /** UPDATE EQUATION
@@ -102,6 +114,24 @@ public class MainActivity extends AppCompatActivity {
     public void updateEquation(View view) {
         TextView textView = (TextView)findViewById(R.id.textView);
         textView.setText(equation);
+
+    }
+
+    public void clearEquation(View view) {
+
+        // Clear all variables
+        equation = "";
+        opnd1 = "";
+        opnd2 = "";
+        operator = "";
+
+        result = 0;
+        remainder = 0;
+
+        isEval = false;
+        isOp = false;
+
+        updateEquation(view);
 
     }
 
