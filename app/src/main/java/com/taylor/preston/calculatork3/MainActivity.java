17db0b55a498685c879fb46078ed32106c207510
prestaylor51 <prestaylor51@gmail.com>
2017-06-15 12:34:12 -0600

    got color, can't figure out spacing

--- app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
+++ app/src/main/java/com/taylor/preston/calculatork3/MainActivity.java
@@ -27,8 +27,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
 
     public void handleButton(View view) {
